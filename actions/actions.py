dc9a1feb5562f6904aad9c37c692f29a1e69d928
Jeevita7812 <justjeevan07@gmail.com>
2020-11-24 20:33:27 +0530

    sorry, there was some error with my original repo

---
+++ actions/actions.py
@@ -0,0 +1,28 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_set_rooms"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        for blob in tracker.latest_message['entities']:
+            if blob['entity'] == 'count_rooms':
+                num = str(blob['value'])
+                dispatcher.utter_message(template="utter_room_types")
+                return [SlotSet("rooms",num)]
